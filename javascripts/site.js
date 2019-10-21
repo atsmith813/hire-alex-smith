fc2627f38f9090057a350c257267fee5d993e984
Alex <alexandersmith223@gmail.com>
2019-10-21 14:21:06 -0700

    Automated commit at 2019-10-21 21:21:06 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,34 @@
+//= require jquery
+//= require popper.js
+//= require bootstrap
+
+$(document).ready(function() {
+  var type_text = [
+    'Hard-working',
+    'Genuine',
+    'Unanticipated',
+    'Smurff',
+    'Honest',
+    'Giggly',
+    'Driven',
+    'Determined',
+    'Smart',
+    'Blunt',
+    'Kind',
+    'Loyal',
+    'Stubborn',
+    'Goofy',
+    'Pessimistic',
+    'Cheesy',
+    'Annoying'
+  ];
+
+  new TypeIt('#type-text', {
+    strings: type_text,
+    breakLines: false,
+    lifeLike: true,
+    loop: true
+  });
+
+  $('.tooltipped').tooltip();
+});
